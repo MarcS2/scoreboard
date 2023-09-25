1f0fe914d9dd490bc3c914eb32446eb37abcbc5d
Marcello Sharrai <marcellosharrai@hotmail.com>
2023-09-25 16:13:19 -0600

    scoreboard

--- app.js
+++ app.js
@@ -7,37 +7,63 @@ let away = 0
 
 
 // SECTION functions
-function homeScore() {
-  const score = document.getElementById('homePoints')
-  home++
-  // @ts-ignore
-  score.innerText = home
-  console.log(home)
+
+function resetScore() {
+  const hPoint = document.getElementById('homePoints')
+  const aPoint = document.getElementById('awayPoints')
+  home = 0
+  away = 0
+
+  hPoint.innerText = home
+  aPoint.innerText = away
 
 }
 
+function checkWin() {
+  if (home >= 21) {
+    window.alert('Home Wins!!')
+  } else if (away >= 21) {
+    window.alert('Away Wins!!')
+  }
 
-function awayScore() {
-  const score = document.getElementById('awayPoints')
-  away++
-  // @ts-ignore
-  score.innerText = away
-  console.log(away)
 }
 
-function bigAwayScore() {
-  const score = document.getElementById('awayPoints')
-  away += 3
-  // @ts-ignore
-  score.innerText = away
-  console.log(away)
+function updateScore(teamName) {
+  if (teamName == 'home') {
+    const score = document.getElementById('homePoints')
+    home++
+    // @ts-ignore
+    score.innerText = home
+    console.log(home)
+    checkWin()
+
+  } else if (teamName == 'away') {
+    const score = document.getElementById('awayPoints')
+    away++
+    // @ts-ignore
+    score.innerText = away
+    console.log(away)
+    checkWin()
+  }
+
 }
 
-function bigHomeScore() {
-  const score = document.getElementById('homePoints')
-  home += 3
-  score.innerText = home
-  console.log(home)
+function updateBigScore(teamName) {
+  if (teamName == 'home') {
+    const score = document.getElementById('homePoints')
+    home += 3
+    score.innerText = home
+    console.log(home)
+    checkWin()
+  } else if (teamName == 'away') {
+    const score = document.getElementById('awayPoints')
+    away += 3
+    score.innerText = away
+    console.log(home)
+    checkWin()
+  }
 }
 
 // !SECTION
+
+
