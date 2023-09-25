89df05383129a9159de3493655c4bb2446c1f19e
Marcello Sharrai <marcellosharrai@hotmail.com>
2023-09-25 15:17:07 -0600

    scoreboard

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// SECTION variables
+// SECTION global scope variables
 let home = 0
 let away = 0
 
@@ -8,7 +8,36 @@ let away = 0
 
 // SECTION functions
 function homeScore() {
-  console.log('Button clicked')
+  const score = document.getElementById('homePoints')
+  home++
+  // @ts-ignore
+  score.innerText = home
+  console.log(home)
+
+}
+
+
+function awayScore() {
+  const score = document.getElementById('awayPoints')
+  away++
+  // @ts-ignore
+  score.innerText = away
+  console.log(away)
+}
+
+function bigAwayScore() {
+  const score = document.getElementById('awayPoints')
+  away += 3
+  // @ts-ignore
+  score.innerText = away
+  console.log(away)
+}
+
+function bigHomeScore() {
+  const score = document.getElementById('homePoints')
+  home += 3
+  score.innerText = home
+  console.log(home)
 }
 
 // !SECTION
